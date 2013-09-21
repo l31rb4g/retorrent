d4c56dabf3e73e82495b55720085244ee2980cb6
Gabriel Paladino <l31rb4g@gmail.com>
2013-09-21 11:40:12 -0300

    gogo

--- html/inc/functions.php
+++ html/inc/functions.php
@@ -3032,6 +3032,7 @@ function getDirListAjax($dirName)
     closedir($handle);
 
     // Now spit out the junk
+	/*
     echo "<table bgcolor=\"".$cfg["table_data_bg"]."\" width=\"100%\" bordercolor=\"".$cfg["table_border_dk"]."\" border=1 cellpadding=3 cellspacing=0>";
 
     if (sizeof($arUserTorrent) > 0)
@@ -3063,6 +3064,9 @@ function getDirListAjax($dirName)
             echo $torrentrow;
         }
     }
+	*/
+
+	return $res;
 }
 
 
